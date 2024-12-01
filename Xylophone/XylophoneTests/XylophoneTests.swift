54140f89c1b85f2f82cdb528e0d8923d0816cf45
Kin Yuunan <yuunan.kin@gmail.com>
2024-12-02 08:58:53 +0900

    Xylophone

---
+++ Xylophone/XylophoneTests/XylophoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneTests.swift
+//  XylophoneTests
+//
+//  Created by Yuunan kin on 2024/12/01.
+//
+
+import Testing
+@testable import Xylophone
+
+struct XylophoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
