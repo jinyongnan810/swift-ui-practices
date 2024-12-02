0c6e19c0b762d0f8d39b6aaf6aab03a7ed73469e
Kin Yuunan <yuunan.kin@gmail.com>
2024-12-02 19:29:11 +0900

    egg timer, play sound

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -5,20 +5,30 @@
 //  Created by Yuunan kin on 2024/12/02.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
     var timer: Timer?
     var elapsedTime: Int = 0
     var totalTime: Int = 0
+    var audioPlayer: AVAudioPlayer?
+    var audioUrl: URL?
 
     let boilTime: [String: Int] = [
-        "Soft": 300,
+        "Soft": 5,
         "Medium": 420,
         "Hard": 720,
     ]
 
     @IBOutlet var progressBar: UIProgressView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        try? AVAudioSession.sharedInstance().setActive(true)
+        audioUrl = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+    }
 
     @IBAction func onPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
@@ -42,6 +52,11 @@ class ViewController: UIViewController {
         print("elapsed time: \(elapsedTime)")
         if elapsedTime >= totalTime {
             stopTimer()
+            DispatchQueue.main.asyncAfter(
+                deadline: .now() + 0.1
+            ) {
+                self.playAudio()
+            }
             let alert = UIAlertController(
                 title: "Done",
                 message: "Your egg is ready!",
@@ -56,5 +71,16 @@ class ViewController: UIViewController {
     func stopTimer() {
         timer?.invalidate()
         timer = nil
+    }
+
+    private func playAudio() {
+        guard let audioUrl else { return }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl, fileTypeHint: AVFileType
+                .mp3.rawValue)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing audio: \(error)")
+        }
     }
 }
