4f810ad075d99610f5d50a6bfb600f9fc774e4cf
Kin Yuunan <yuunan.kin@gmail.com>
2024-12-08 10:15:23 +0900

    clima part1

---
+++ Clima/ClimaTests/ClimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaTests.swift
+//  ClimaTests
+//
+//  Created by Yuunan kin on 2024/12/07.
+//
+
+import Testing
+@testable import Clima
+
+struct ClimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
