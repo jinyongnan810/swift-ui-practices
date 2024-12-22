96879f02a650a3f2d541655db183197d995fb553
Kin Yuunan <yuunan.kin@gmail.com>
2024-12-23 08:25:57 +0900

    todoey starter

---
+++ Todoey/Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Yuunan kin on 2024/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
