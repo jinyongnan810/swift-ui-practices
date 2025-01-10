1a964a26cb70bb21680f73c1fb88a03d56b55804
Kin Yuunan <yuunan.kin@gmail.com>
2025-01-10 22:26:36 +0900

    add title

--- Greetings/Greetings/ContentView.swift
+++ Greetings/Greetings/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     let messages: [MessageItemModel] = [
         .init(text: "Hello World", color: .cyan),
-        .init(text: "SwiftUI Rocks!!!!!", color: .red),
+        .init(text: "SwiftUI Rocks!!!!!!!!!!!!!!!!", color: .red),
         .init(text: "Are you there?", color: .yellow),
         .init(text: "Start coding now.", color: .green),
         .init(text: "Boom.", color: .purple),
@@ -25,9 +25,20 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             VStack(alignment: .leading) {
+                VStack(alignment: .leading) {
+                    Text("Greetings")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    Text("Explore SwiftUI")
+                        .font(.headline)
+                        .fontWeight(.light)
+                }
+                Spacer()
                 ForEach(messages) { message in
                     MessageView(text: message.text, color: message.color)
                 }
+                Spacer()
+                Spacer()
             }.padding()
 
         }
