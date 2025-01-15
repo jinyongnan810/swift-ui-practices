24e292482b0fd853200d5417cd9f1ec86fe11482
Kin Yuunan <yuunan.kin@gmail.com>
2025-01-15 14:05:58 +0900

    customize tabs

--- SwiftUIPractices/SwiftUIPractices/Examples/TabViewExample/TabViewExample.swift
+++ SwiftUIPractices/SwiftUIPractices/Examples/TabViewExample/TabViewExample.swift
@@ -13,19 +13,33 @@ enum TabType {
 
 struct TabViewExample: View {
     @State private var selectedTab: TabType = .movies
+    @State var customization = TabViewCustomization()
     var body: some View {
         TabView(selection: $selectedTab) {
             Tab("Movies", systemImage: "play", value: .movies) {
                 Text("Movie list")
             }
+            .customizationID("com.myApp.Movies")
             Tab("Books", systemImage: "book.pages.fill", value: .books) {
                 Text("Book list")
             }
+            .customizationID("com.myApp.Books")
             Tab(value: .search, role: .search) {
                 Text("Search movies or books")
             }
+            .customizationID("com.myApp.Search")
+            // currently it seems like selection and TabSection is not compatable
+//            TabSection("Hobbies") {
+//                Tab("Tab 1", systemImage: "person", value: .music) {
+//                    Text("Tab 1 View")
+//                }
+//                Tab("Tab 2", systemImage: "trash", value: .meditation) {
+//                    Text("Tab 2 View")
+//                }
+//            }
         }
         .tabViewStyle(.sidebarAdaptable)
+        .tabViewCustomization($customization)
         .onChange(of: selectedTab) { previous, next in
             print("⭐️ selectedTab changed from \(previous) to \(next)")
         }
