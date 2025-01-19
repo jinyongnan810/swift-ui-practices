b5484cf1655783bb94f2ca8767a06e08200814d8
Kin Yuunan <yuunan.kin@gmail.com>
2025-01-19 10:52:24 +0900

    update sf symbol example

--- SwiftUIPractices/SwiftUIPractices/Examples/AnimationExample/SFSymbolsAnimation.swift
+++ SwiftUIPractices/SwiftUIPractices/Examples/AnimationExample/SFSymbolsAnimation.swift
@@ -8,72 +8,96 @@
 import SwiftUI
 
 struct SFSymbolsAnimation: View {
-    @State private var clicked = false
+    @State private var section1 = false
     @State private var pauseGlobe = false
+    @State private var section2 = false
+    @State private var section3 = false
+    @State private var section4 = false
+    @State private var section5 = false
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .symbolEffectsRemoved(pauseGlobe)
-                    .symbolEffect(.bounce, value: clicked)
-                    .padding()
-                Image(systemName: "house.fill")
-                    .imageScale(.large)
-                    .symbolEffect(.pulse, value: clicked)
-                    .padding()
-                Image(systemName: "square.stack.3d.up")
-                    .imageScale(.large)
-                    .symbolEffect(.variableColor.iterative, value: clicked)
-                    .foregroundStyle(clicked ? .red : .black)
-                    .padding()
-                Image(systemName: "square.stack.3d.up")
-                    .imageScale(.large)
-                    .symbolEffect(.variableColor.cumulative, value: clicked)
-                    .foregroundStyle(clicked ? .blue : .black)
-                    .padding()
-            }
-            HStack {
-                Image(systemName: clicked ? "eye" : "eye.slash")
-                    .imageScale(.large)
-                    .symbolEffect(.bounce, value: clicked)
-                    .foregroundStyle(clicked ? .blue : .black)
-                    .padding()
-                Image(systemName: clicked ? "eye" : "eye.slash")
-                    .imageScale(.large)
-                    .symbolEffect(.bounce, value: clicked)
-                    .foregroundStyle(clicked ? .blue : .black)
-                    .contentTransition(.symbolEffect(.replace.byLayer.downUp))
-                    .padding()
-                Image(systemName: clicked ? "person.crop.circle.badge.plus" : "bolt.horizontal.circle.fill")
-                    .imageScale(.large)
-                    .symbolEffect(
-                        .pulse.wholeSymbol,
-                        options: .repeat(3).speed(3), value: clicked
-                    )
-                    .foregroundStyle(clicked ? .blue : .purple)
-                    .padding()
-            }
-            HStack {
-                Button {
-                    clicked.toggle()
-                } label: {
-                    Text("animate")
-                        .font(.headline)
-                        .foregroundColor(.orange)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 20).fill(.black))
+        Form {
+            Section("Enable and disable animations") {
+                HStack {
+                    Image(systemName: "globe")
+                        .symbolEffectsRemoved(pauseGlobe)
+                        .symbolEffect(.bounce, value: section1)
+                    Image(systemName: "house.fill")
+                        .symbolEffect(.pulse, value: section1)
                 }
 
-                Button {
+                Button(pauseGlobe ? "resume globe animation" : "pause globe animation") {
                     pauseGlobe.toggle()
-                } label: {
-                    Text(pauseGlobe ? "resume globe animation" : "pause globe animation")
-                        .font(.headline)
-                        .foregroundColor(pauseGlobe ? .cyan : .orange)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 20).fill(.black))
                 }
+            }.onTapGesture {
+                section1.toggle()
+            }
+            Section("iterative and cumulative") {
+                HStack {
+                    Image(systemName: "square.stack.3d.up")
+                        .symbolEffect(.variableColor.iterative, value: section2)
+                        .foregroundStyle(section2 ? .red : .black)
+                    Image(systemName: "square.stack.3d.up")
+                        .symbolEffect(.variableColor.cumulative, value: section2)
+                        .foregroundStyle(section2 ? .blue : .black)
+                }
+            }.onTapGesture {
+                section2.toggle()
+            }
+            Section("Transition") {
+                HStack {
+                    Image(systemName: section3 ? "eye" : "eye.slash")
+                        .symbolEffect(.bounce, value: section3)
+                        .foregroundStyle(section3 ? .blue : .black)
+                    Image(systemName: section3 ? "eye" : "eye.slash")
+                        .symbolEffect(.bounce, value: section3)
+                        .foregroundStyle(section3 ? .blue : .black)
+                        .contentTransition(.symbolEffect(.replace.byLayer.downUp))
+                }
+            }.onTapGesture {
+                section3.toggle()
+            }
+
+            Section("Repeat and Speed") {
+                HStack {
+                    Image(systemName: section4 ? "person.crop.circle.badge.plus" : "bolt.horizontal.circle.fill")
+                        .symbolEffect(
+                            .pulse.wholeSymbol,
+                            options: .repeat(3).speed(3), value: section4
+                        )
+                        .foregroundStyle(section4 ? .blue : .purple)
+                }
+            }
+            .onTapGesture {
+                section4.toggle()
+            }
+
+            Section("RenderingMode and direction") {
+                HStack {
+                    Image(systemName: "leaf.arrow.trianglehead.clockwise")
+                        .symbolRenderingMode(.monochrome)
+                        .symbolEffect(.wiggle, value: section5)
+                    Image(systemName: "leaf.arrow.trianglehead.clockwise")
+                        .symbolRenderingMode(.hierarchical)
+                        .symbolEffect(.wiggle.forward.byLayer, value: section5)
+                    Image(systemName: "leaf.arrow.trianglehead.clockwise")
+                        .symbolRenderingMode(.multicolor)
+                        .symbolEffect(.wiggle.down.byLayer, value: section5)
+                    Image(systemName: "leaf.arrow.trianglehead.clockwise")
+                        .symbolRenderingMode(.multicolor)
+                        .symbolEffect(
+                            .rotate.clockwise.byLayer,
+                            value: section5
+                        )
+                    Image(systemName: "leaf.arrow.trianglehead.clockwise")
+                        .symbolRenderingMode(.multicolor)
+                        .symbolEffect(
+                            .rotate.clockwise.wholeSymbol,
+                            value: section5
+                        )
+                }
+            }
+            .onTapGesture {
+                section5.toggle()
             }
         }
     }
