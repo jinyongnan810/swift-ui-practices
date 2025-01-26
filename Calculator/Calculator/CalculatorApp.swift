86ced5886a925dbf41ac172a8ee650b2c0b3e337
Kin Yuunan <yuunan.kin@gmail.com>
2025-01-27 08:21:33 +0900

    calculator initial

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Yuunan kin on 2025/01/27.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
