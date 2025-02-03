b1aed6ede370ebaa18b4b04fdd332395f792cd69
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-03 13:19:15 +0900

    Charts Starter

---
+++ ChartsPractices/ChartsPractices/ChartsPracticesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsPracticesApp.swift
+//  ChartsPractices
+//
+//  Created by Yuunan kin on 2025/02/03.
+//
+
+import SwiftUI
+
+@main
+struct ChartsPracticesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
