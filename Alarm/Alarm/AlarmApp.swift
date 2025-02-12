8adf61387aade4f2a3976bd17904fe8574db7dfc
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-12 16:15:58 +0900

    initial

---
+++ Alarm/Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Yuunan kin on 2025/02/12.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AlarmView()
+        }
+    }
+}
---
+++ Alarm/Alarm/View/AlarmView.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmView.swift
+//  Alarm
+//
+//  Created by Yuunan kin on 2025/02/12.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AlarmView()
+}
