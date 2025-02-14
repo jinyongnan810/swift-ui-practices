c0a92aee735fe81e598d125320b5f1e88e288ab7
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-14 20:24:46 +0900

    update color name

--- Alarm/Alarm/Constants/Color Constants.swift
+++ Alarm/Alarm/Constants/Color Constants.swift
@@ -1,18 +1,13 @@
 
 import SwiftUI
 
-let blue = Color("blue")
-let pink = Color("pink")
-let purple = Color("purple")
-let yellow = Color("yellow")
-let darkYellow = Color("darkYellow")
-let lightYellow = Color("lightYellow")
-let backgroundColor = Color("backgroundColor")
-let cardBackgroundColor = Color("cardBackgroundColor")
-let lightGray = Color("lightGray")
-let black = Color("black")
-let nickel = Color("nickel")
+let gradColorList: [Color] = [.myYellow, .myPink, .myPurple, .myBlue]
 
-let gradColorList = [yellow, pink, purple, blue]
-
-let mainColors = [pink, purple, blue, yellow, lightGray, black]
+let mainColors: [Color] = [
+    .myPink,
+    .myPurple,
+    .myBlue,
+    .myYellow,
+    .myLightGray,
+    .myBlack
+]
---
+++ Alarm/Alarm/View/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  Alarm
+//
+//  Created by Yuunan kin on 2025/02/14.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
