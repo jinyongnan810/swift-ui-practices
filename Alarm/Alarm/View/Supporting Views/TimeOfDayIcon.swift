f16041bf8d30c9c48e0b51563f1181506ba7db44
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-14 16:12:07 +0900

    language color and assets

---
+++ Alarm/Alarm/View/Supporting Views/TimeOfDayIcon.swift
@@ -0,0 +1,18 @@
+//
+//  TimeOfDayIcon.swift
+//  Alarm
+//
+//  Created by Yuunan kin on 2025/02/14.
+//
+
+import SwiftUI
+
+struct TimeOfDayIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeOfDayIcon()
+}
