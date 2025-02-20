c433467c0c8d161212025ffda9a0e62a44fc038e
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-20 19:46:08 +0900

    Drag Arc

--- Alarm/Alarm/Time/TimeAndDateHelper.swift
+++ Alarm/Alarm/Time/TimeAndDateHelper.swift
@@ -12,6 +12,49 @@ func currentDayWithHourSet(_ hour: Int) -> Date {
         .date(bySettingHour: hour, minute: 0, second: 0, of: Date())!
 }
 
+func posToDegree(_ pos: CGPoint) -> CGFloat {
+    let x = pos.x
+    let y = pos.y
+    if x == 0 || y == 0 {
+        if x == 0, y > 0 {
+            return 180
+        } else if x == 0, y < 0 {
+            return 0
+        } else if x > 0, y == 0 {
+            return 90
+        } else if x < 0, y == 0 {
+            return 270
+        } else {
+            return 0
+        }
+    }
+    let r = sqrt(x * x + y * y)
+    let cosValue = (pow(x, 2) + pow(r, 2) - pow(y, 2)) / (2 * abs(x) * r)
+    let acosValue = acos(cosValue)
+    let angleBetweenXAndR = acosValue * 180 / .pi
+    // print("⭐️ x: \(x), y: \(y), r: \(r), cosValue: \(cosValue), acosValue: \(acosValue), angleBetweenXAndR: \(angleBetweenXAndR)")
+    if x > 0, y > 0 {
+        return 90 + angleBetweenXAndR
+    } else if x < 0, y > 0 {
+        return 270 - angleBetweenXAndR
+    } else if x < 0, y < 0 {
+        return 270 + angleBetweenXAndR
+    } else {
+        return 90 - angleBetweenXAndR
+    }
+}
+
+func degreeToTime(_ degree: CGFloat) -> Date {
+    let percent = degree / 360
+    let timeInMinutes = 24 * 60 * percent
+    let hour = Int(timeInMinutes) / 60
+    let minute = Int(timeInMinutes) % 60
+    var dateComponents = DateComponents()
+    dateComponents.hour = hour
+    dateComponents.minute = minute
+    return Calendar.current.date(from: dateComponents)!
+}
+
 extension Date {
     static func - (lhs: Date, rhs: Date) -> TimeInterval {
         .init(lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate)
--- Alarm/Alarm/View/Supporting Views/ArcView.swift
+++ Alarm/Alarm/View/Supporting Views/ArcView.swift
@@ -31,6 +31,16 @@ struct ArcView: View {
         Angle(degrees: 360 * startPercent - 90)
     }
 
+    var iconDragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                let location = value.location
+                let degree = posToDegree(location)
+                let time = degreeToTime(degree).fullDateString()
+//                print("⭐️ \(location), \(degree), \(time)")
+            }
+    }
+
     var body: some View {
         ZStack {
             Circle()
@@ -58,8 +68,24 @@ struct ArcView: View {
                 .trim(from: 0, to: diff)
                 .stroke(.white, style: .init(lineWidth: 15, dash: [1, 2]))
                 .rotationEffect(rotation)
-            LineCapIcon(time: start)
-            LineCapIcon(time: end)
+            LineCapIcon(time: start).gesture(
+                DragGesture()
+                    .onChanged { value in
+                        let location = value.location
+                        let degree = posToDegree(location)
+                        let time = degreeToTime(degree)
+                        start = time
+                    }
+            )
+            LineCapIcon(time: end).gesture(
+                DragGesture()
+                    .onChanged { value in
+                        let location = value.location
+                        let degree = posToDegree(location)
+                        let time = degreeToTime(degree)
+                        end = time
+                    }
+            )
 
         }.frame(width: width)
     }
