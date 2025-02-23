1da0c890c114935784c58043d7dd11e7fe6a7960
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-24 08:09:07 +0900

    starter

---
+++ Game Of Life/Game Of Life/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Game Of Life
+//
+//  Created by Yuunan kin on 2025/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
