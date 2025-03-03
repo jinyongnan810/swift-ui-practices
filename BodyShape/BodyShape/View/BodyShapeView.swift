6a236b997cab92e09e003c1c79b06af82df57abd
Kin Yuunan <yuunan.kin@gmail.com>
2025-03-03 17:25:24 +0900

    Splash

--- BodyShape/BodyShape/View/BodyShapeView.swift
+++ BodyShape/BodyShape/View/BodyShapeView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct BodyShapeView: View {
+    @State private var displayingSplashScreen: Bool = true
+    @State private var blurRadius: CGFloat = 5
+
     var body: some View {
-        DashBoardView()
+        ZStack {
+            if displayingSplashScreen {
+                SplashView()
+                    .opacity(displayingSplashScreen ? 1 : 0)
+                    .blur(radius: blurRadius)
+                    .onTapGesture {
+                        withAnimation(.easeIn(duration: 0.5)) {
+                            blurRadius = 5
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            withAnimation(.easeIn(duration: 0.5)) {
+                                displayingSplashScreen = false
+                            }
+                        }
+                    }
+            } else {
+                DashBoardView()
+            }
+        }.onAppear {
+            withAnimation(.easeIn(duration: 0.5)) {
+                blurRadius = 0
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation(.easeIn(duration: 0.5)) {
+                    displayingSplashScreen = false
+                }
+            }
+        }
     }
 }
 
