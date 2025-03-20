387d8dd5d0b8f994b084ad3b17a9b30ff3b6af49
Kin Yuunan <yuunan.kin@gmail.com>
2025-03-20 12:42:33 +0900

    flip v2

--- SwiftUIPractices/SwiftUIPractices/Practices/FlipCard/FlipCardPractice.swift
+++ SwiftUIPractices/SwiftUIPractices/Practices/FlipCard/FlipCardPractice.swift
@@ -9,25 +9,32 @@ import SwiftUI
 
 struct FlipCardPractice: View {
     @State private var isFlipped = false
-    var sideAZIndex: Double { isFlipped ? 1 : 2 }
-    var sideBZIndex: Double { isFlipped ? 2 : 1 }
+//    var sideAZIndex: Double { isFlipped ? 1 : 2 }
+//    var sideBZIndex: Double { isFlipped ? 2 : 1 }
+    var sideAOpacity: Double { isFlipped ? 0 : 1 }
+    var sideBOpacity: Double { isFlipped ? 1 : 0 }
     var body: some View {
         ZStack {
             SideA()
-                .zIndex(sideAZIndex)
+//                .zIndex(sideAZIndex)
+                .opacity(sideAOpacity)
+                .animation(.easeInOut, value: sideAOpacity)
             SideB()
                 .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
-                .zIndex(sideBZIndex)
-        }.frame(width: 300, height: 400)
-            .clipShape(.rect(cornerRadius: 12))
-            .shadow(radius: 5)
-            .rotation3DEffect(.degrees(isFlipped ? 180: 0), axis: (x: 0, y: 1, z: 0))
-            .onTapGesture {
+//                .zIndex(sideBZIndex)
+                .opacity(sideBOpacity)
+                .animation(.easeInOut, value: sideBOpacity)
+        }
+        .frame(width: 300, height: 400)
+        .clipShape(.rect(cornerRadius: 12))
+        .shadow(radius: 5)
+        .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 0, y: 1, z: 0), perspective: 0.8)
+        .onTapGesture {
 //                withAnimation(.spring(duration: 2)) {
-                withAnimation {
-                    isFlipped.toggle()
-                }
+            withAnimation {
+                isFlipped.toggle()
             }
+        }
     }
 }
 
