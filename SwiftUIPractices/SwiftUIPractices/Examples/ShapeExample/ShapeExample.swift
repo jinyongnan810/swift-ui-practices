679491573c842eac6cdebbe92194ab9bcc196b2e
Kin Yuunan <yuunan.kin@gmail.com>
2025-03-20 12:11:40 +0900

    add shape

--- SwiftUIPractices/SwiftUIPractices/Examples/ShapeExample/ShapeExample.swift
+++ SwiftUIPractices/SwiftUIPractices/Examples/ShapeExample/ShapeExample.swift
@@ -9,19 +9,43 @@ import SwiftUI
 
 struct ShapeExample: View {
     @State private var startAngle: CGFloat = 0
+    @State private var waveHeight: CGFloat = 200
+    @State private var waveOffset: CGFloat = 0
     let timer = Timer.publish(every: 0.01, on: .main, in: .common)
         .autoconnect()
 
     var body: some View {
-        Image("imgNature")
-            .resizable()
-            .frame(width: 400, height: 400)
-            .clipShape(
-                StarShape(startAngle: startAngle)
-            )
-            .onReceive(timer) { _ in
-                startAngle += 0.01
+        ZStack {
+            Image("imgNature")
+                .resizable()
+                .frame(width: 400, height: 400)
+                .clipShape(
+                    StarShape(startAngle: startAngle)
+                )
+
+            VStack {
+                Spacer()
+                WaveShape()
+                    .fill(
+                        LinearGradient(
+                            colors: [.green, .white],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                    .rotationEffect(.degrees(180))
+                    .frame(height: waveHeight)
             }
+        }
+        .onReceive(timer) { _ in
+            startAngle += 0.01
+            waveHeight = sin(startAngle) * 40 + 200
+            waveOffset += 0.01
+            if waveOffset > 1 {
+                waveOffset = 0
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
@@ -79,6 +103,33 @@ struct StarShape: Shape {
         }
         path.closeSubpath()
         return path
+    }
+}
+
+struct WaveShape: Shape {
+    var offset: CGFloat = 0
+    func path(in rect: CGRect) -> Path {
+        // TODO: implement wave
+        var offset1 = 0.25 + offset
+        offset1 = offset1 - CGFloat(Int(offset1))
+        var offset2 = 0.75 + offset
+        offset2 = offset2 - CGFloat(Int(offset2))
+        let control1Offset: CGFloat = offset1 * rect.maxX
+        let control2Offset: CGFloat = offset2 * rect.maxX
+        return Path { path in
+            path.move(to: CGPoint(x: 0, y: rect.midY))
+            path
+                .addCurve(
+                    to: .init(x: rect.maxX, y: rect.midY),
+                    control1: .init(
+                        x: control1Offset,
+                        y: 0
+                    ),
+                    control2: .init(x: control2Offset, y: rect.maxY)
+                )
+            path.addLine(to: .init(x: rect.maxX, y: 0))
+            path.addLine(to: .init(x: 0, y: 0))
+        }
     }
 }
 
