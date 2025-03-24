efa68bb7317b42f20136d23d7b31faef235b134c
Kin Yuunan <yuunan.kin@gmail.com>
2025-03-24 18:26:28 +0900

    initial

---
+++ camera-practices/camera-practices/camera_practicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_practicesApp.swift
+//  camera-practices
+//
+//  Created by Yuunan kin on 2025/03/24.
+//
+
+import SwiftUI
+
+@main
+struct camera_practicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
