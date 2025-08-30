442025482610410162930629b4d5691c056ccc39
Kin Yuunan <yuunan.kin@gmail.com>
2025-08-30 20:27:28 +0900

    draw lines

--- spider-effect/spider-effect/ContentView.swift
+++ spider-effect/spider-effect/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var dots: [CGPoint]
     let dotCount = 100
     let dotRadius: CGFloat = 5
+    let centerDotRaidus: CGFloat = 10
+
+    @State var dragPosition: CGPoint?
+    let influence: CGFloat = 0.15
+
     init() {
         dots = (0 ..< dotCount).map { _ in
             CGPoint(x: CGFloat.random(in: 0 ... 1), y: CGFloat.random(in: 0 ... 1))
@@ -23,16 +28,60 @@ struct ContentView: View {
             Canvas {
                 ctx,
                     size in
+
+                guard let dragPosition else { return }
+
+                // center dot
+                let centerDot = CGRect(
+                    x: dragPosition.x - centerDotRaidus,
+                    y: dragPosition.y - centerDotRaidus,
+                    width: centerDotRaidus * 2,
+                    height: centerDotRaidus * 2
+                )
+                ctx.fill(Path(ellipseIn: centerDot), with: .color(.white))
+
+                // dots and lines
                 for dot in dots {
+                    let dotCenter = CGPoint(
+                        x: size.width * dot.x,
+                        y: size.height * dot.y
+                    )
+                    let dotx = dotCenter.x - dotRadius
+                    let doty = dotCenter.y - dotRadius
+                    let distanceToCenter = hypot(
+                        centerDot.origin.x - dotx,
+                        centerDot.origin.y - doty
+                    )
+
+                    if influence < (distanceToCenter / size.height) {
+                        continue
+                    }
+
+                    // dots
                     let rect = CGRect(
-                        x: size.width * dot.x - dotRadius,
-                        y: size.height * dot.y - dotRadius,
+                        x: dotx,
+                        y: doty,
                         width: dotRadius * 2,
                         height: dotRadius * 2
                     )
                     ctx.fill(Path(ellipseIn: rect), with: .color(.white))
+
+                    // lines
+                    var line = Path()
+                    line.move(to: CGPoint(x: dragPosition.x, y: dragPosition.y))
+                    line.addLine(to: dotCenter)
+                    ctx.stroke(line, with: .color(.white))
                 }
             }
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                        dragPosition = value.location
+                    }
+                    .onEnded { _ in
+                        dragPosition = nil
+                    }
+            )
         }
     }
 }
